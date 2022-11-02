1b8a0be260220fc76db3d661b5e39d50bfcc7c7e
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-02 11:53:43 +0300

    get api successfully created for all data form mongodb database

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
+
 
 //middleware
 app.use(cors());
@@ -10,6 +13,31 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Smart car sever is running')
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jjvuikj.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('smartCar').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+    } finally {
+
+    }
+
+}
+run().catch(err => console.error(err))
+
 
 app.listen(port, () => {
     console.log(`Smart car project running on${port}`)
